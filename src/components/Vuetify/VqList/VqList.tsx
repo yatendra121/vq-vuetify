7c6a2db83a92896a37d6fc6d52acc11c3f4384b6
Yatendra <yatendra121kushwaha@gmail.com>
2023-07-12 11:15:01 +0530

    fixed type issue

--- src/components/Vuetify/VqList/VqList.tsx
+++ src/components/Vuetify/VqList/VqList.tsx
@@ -13,17 +13,17 @@ import {
     toRef,
     h,
     VNode
-} from 'vue'
+} from "vue";
 
-import { VList } from 'vuetify/components'
-import axios, { CancelTokenSource } from 'axios'
-import { useAsyncAxios, objectToQueryString } from '@qnx/composables/axios'
-import { useListRepository } from '../../../composables/list'
-import { useFormFilterStore } from '../../../store/reactivity/formFiler'
+import { VList } from "vuetify/components";
+import axios, { CancelTokenSource } from "axios";
+import { useAsyncAxios, objectToQueryString } from "@qnx/composables/axios";
+import { useListRepository } from "../../../composables/list";
+import { useFormFilterStore } from "../../../store/reactivity/formFiler";
 
-type TValue = unknown
+type TValue = unknown;
 export const VqList = defineComponent({
-    name: 'VqList',
+    name: "VqList",
     props: {
         id: {
             type: String as PropType<string>,
@@ -43,114 +43,114 @@ export const VqList = defineComponent({
     },
     setup(props, { attrs, slots }) {
         const filterId = computed(() => {
-            return `${props.id}_filter`
-        })
-        provide('tableListId', readonly(toRef(props, 'id')))
+            return `${props.id}_filter`;
+        });
+        provide("tableListId", readonly(toRef(props, "id")));
 
         //Collect global values of list based on filter id
-        const { removeList, collectListValues } = useListRepository(filterId.value)
-        const { items, finished, loading } = toRefs(collectListValues<TValue>())
+        const { removeList, collectListValues } = useListRepository(filterId.value);
+        const { items, finished, loading } = toRefs(collectListValues<TValue>());
 
         onBeforeUnmount(() => {
-            removeList()
-        })
+            removeList();
+        });
 
         const listOptions = reactive({
             page: 1,
             page_size: props.pageSize
-        })
+        });
 
-        const formFilterStore = useFormFilterStore()
-        const { setReloadValue } = useFormFilterStore()
+        const formFilterStore = useFormFilterStore();
+        const { setReloadValue } = useFormFilterStore();
 
         const formFilterData = computed<Object>(() => {
-            return formFilterStore.forms[filterId.value]?.values
-        })
+            return formFilterStore.forms[filterId.value]?.values;
+        });
 
         const reloadRequired = computed<boolean>(() => {
-            return formFilterStore.forms[filterId.value]?.reloadRequired ?? false
-        })
+            return formFilterStore.forms[filterId.value]?.reloadRequired ?? false;
+        });
 
         const resetRequired = computed<boolean>(() => {
-            return formFilterStore.forms[filterId.value]?.resetRequired ?? false
-        })
+            return formFilterStore.forms[filterId.value]?.resetRequired ?? false;
+        });
 
         watch(reloadRequired, (newVal) => {
             if (newVal) {
-                listOptions.page = 1
-                setReloadValue(filterId.value, false)
-                loadMore()
+                listOptions.page = 1;
+                setReloadValue(filterId.value, false);
+                loadMore();
             }
-        })
+        });
 
         watch(resetRequired, (newVal) => {
             if (newVal) {
-                setReloadValue(filterId.value, false)
-                listOptions.page = 1
-                loadMore()
+                setReloadValue(filterId.value, false);
+                listOptions.page = 1;
+                loadMore();
             }
-        })
+        });
 
         watch(
             () => formFilterData.value,
             //@ts-ignore
             (newVal, oldVal) => {
-                if (oldVal === undefined) return
-                listOptions.page = 1
-                loadMore()
+                if (oldVal === undefined) return;
+                listOptions.page = 1;
+                loadMore();
             },
             { deep: true }
-        )
+        );
 
         const loadMore = () => {
             fetchItems()
                 .then((res) => {
-                    items.value = listOptions.page === 1 ? res.data : [...items.value, ...res.data]
+                    items.value = listOptions.page === 1 ? res.data : [...items.value, ...res.data];
 
-                    finished.value = listOptions.page_size * listOptions.page >= (res?.total ?? 0)
-                    listOptions.page++
+                    finished.value = listOptions.page_size * listOptions.page >= (res?.total ?? 0);
+                    listOptions.page++;
                 })
-                .catch(() => {})
-        }
+                .catch(() => {});
+        };
 
-        let cancelToken: CancelTokenSource
+        let cancelToken: CancelTokenSource;
         const fetchItems = async () => {
             try {
-                cancelToken?.cancel()
-                cancelToken = axios.CancelToken.source()
+                cancelToken?.cancel();
+                cancelToken = axios.CancelToken.source();
 
-                loading.value = true
+                loading.value = true;
 
                 const response = await useAsyncAxios<{
-                    data: { data: any; total: number }
+                    data: { data: any; total: number };
                 }>(
                     `${props.action}?${objectToQueryString(
                         { ...toRaw(listOptions), ...toRaw(formFilterData.value ?? {}) },
-                        ''
+                        ""
                     )}`,
                     {
-                        method: 'GET'
+                        method: "GET"
                     },
                     { cancelToken }
-                )
-                loading.value = false
-                return response.data
+                );
+                loading.value = false;
+                return response.data;
             } catch (e: any) {
-                loading.value = false
-                throw new Error(e.message)
+                loading.value = false;
+                throw new Error(e.message);
             }
-        }
+        };
 
         onMounted(() => {
-            loadMore()
-        })
+            loadMore();
+        });
 
-        provide('vqList', {
+        provide("vqList", {
             loading,
             finished,
             tableListId: props.id,
             loadMore
-        })
+        });
 
         return () => (
             <>
@@ -171,28 +171,28 @@ export const VqList = defineComponent({
           ))} */}
                 </VList>
             </>
-        )
+        );
     }
-})
+});
 
 // eslint-disable-next-line no-redeclare
-export type VqList = typeof VList & typeof VqList
+export type VqList = typeof VList & typeof VqList;
 
 /**
  * Creating composable function types to create strong typed component
  */
 export type ExtractComponentProps<TComponent> = TComponent extends new () => {
-    $props: infer P
+    $props: infer P;
 }
     ? P
-    : never
+    : never;
 
 interface GenericProps
     extends ExtractComponentProps<typeof VqList>,
-        ExtractComponentProps<typeof VList> {}
+        ExtractComponentProps<typeof VqList> {}
 
 interface GenericSlotsProps<TValue> {
-    items: TValue[]
+    items: TValue[];
 }
 
 /**
@@ -203,18 +203,18 @@ export function useVqList<TValue = unknown>() {
         // Returning functions in `setup` means this is the render function
         return () => {
             // Event handlers will also be passed in the `props` object
-            return h(VqList, props, slots)
-        }
-    })
+            return h(VqList, props, slots);
+        };
+    });
     return wrapper as typeof wrapper & {
         new (): {
             // Same trick as `$slots`, we override the emit information for that component
             $emit: {
-                (e: 'changed', value: TValue): void
-            }
+                (e: "changed", value: TValue): void;
+            };
             $slots: {
-                default: (arg: GenericSlotsProps<TValue>) => VNode[]
-            }
-        }
-    }
+                default: (arg: GenericSlotsProps<TValue>) => VNode[];
+            };
+        };
+    };
 }

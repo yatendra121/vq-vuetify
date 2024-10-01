7ef37bce50399235008f61cff3b549e722bc7cf3
Yatendra Kushwaha <yatendra121kushwaha@gmail.com>
2024-10-01 16:53:00 +0530

    testing1

--- src/components/Vuetify/VqTimePicker/index.tsx
+++ src/components/Vuetify/VqTimePicker/index.tsx
@@ -4,65 +4,61 @@ import { VTimePicker } from "vuetify/labs/VTimePicker";
 import { VTextField, VDialog, VMenu } from "vuetify/components";
 
 export const VqTimePicker = defineComponent({
-    name: "VqTimePicker",
-    props: {
-        name: {
-            type: String,
-            required: true
-        },
-        label: {
-            type: String,
-            default: ""
-        },
-        type: {
-            type: Boolean,
-            default: false
-        }
+  name: "VqTimePicker",
+  props: {
+    name: {
+      type: String,
+      required: true,
     },
-    components: {
-        VTimePicker
+    label: {
+      type: String,
+      default: "",
     },
-    setup(props, { attrs, slots }) {
-        const { value, errorMessage } = useField<string | undefined>(
-            toRef(props, "name"),
-            undefined,
-            {
-                validateOnValueUpdate: false
-            }
-        );
+    type: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  components: {
+    VTimePicker,
+  },
+  setup(props, { attrs, slots }) {
+    const { value, errorMessage } = useField<string | undefined>(toRef(props, "name"), undefined, {
+      validateOnValueUpdate: false,
+    });
 
-        const modal = ref(false);
+    const modal = ref(false);
 
-        const ViewComponent = props.type ? VDialog : VMenu;
-        return () => (
-            <>
-                <VTextField
-                    error={!!errorMessage.value}
-                    v-model={value.value}
-                    error-messages={errorMessage.value}
-                    messages={errorMessage.value}
-                    active={modal.value}
-                    focused={modal.value}
-                    label={props.label}
-                    readonly
-                >
-                    <ViewComponent
-                        close-on-content-click={false}
-                        v-model={modal.value}
-                        activator="parent"
-                        width="auto"
-                    >
-                        <VTimePicker
-                            title="Select Time"
-                            format="24hr"
-                            v-model={value.value}
-                            v-slots={slots}
-                            {...attrs}
-                        ></VTimePicker>
-                    </ViewComponent>
-                </VTextField>
-            </>
-        );
-    }
+    const ViewComponent = props.type ? VDialog : VMenu;
+    return () => (
+      <>
+        <VTextField
+          error={!!errorMessage.value}
+          v-model={value.value}
+          error-messages={errorMessage.value}
+          messages={errorMessage.value}
+          active={modal.value}
+          focused={modal.value}
+          label={props.label}
+          readonly
+        >
+          <ViewComponent
+            close-on-content-click={false}
+            v-model={modal.value}
+            activator="parent"
+            width="auto"
+          >
+            <VTimePicker
+              title="Select Time"
+              format="24hr"
+              v-model={value.value}
+              v-slots={slots}
+              {...attrs}
+            ></VTimePicker>
+          </ViewComponent>
+        </VTextField>
+      </>
+    );
+  },
 });
 export type VqTimePicker = typeof VTimePicker & typeof VqTimePicker;
--- vite.config.ts
+++ vite.config.ts
@@ -7,61 +7,47 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-            "@": resolve(__dirname, "./src")
-        }
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
     },
-    plugins: [
-        vue(),
-        vuetify({
-            autoImport: false,
-            styles: "none"
-        }),
-        vueJsx()
-        //dts()
-    ],
-    define: { "process.env": {} },
-    build: {
-        lib: {
-            entry: {
-                index: resolve(__dirname, "src/index.ts"),
-                integrations: resolve(__dirname, "src/integrations.ts")
-            },
-            // name: "vq-vuetify",
-            fileName: (format, entryName) => `${entryName}.js`,
-            formats: ["es"]
+  },
+  plugins: [
+    vue(),
+    vuetify({
+      autoImport: false,
+      styles: "none",
+    }),
+    vueJsx(),
+    //dts()
+  ],
+  define: { "process.env": {} },
+  build: {
+    lib: {
+      entry: {
+        //index: resolve(__dirname, "src/index.ts"),
+        integrations: resolve(__dirname, "src/integrations.ts"),
+      },
+      // name: "vq-vuetify",
+      fileName: (format, entryName) => `${entryName}.js`,
+      formats: ["es"],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [],
+      output: {
+        //format: 'esm',
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
         },
-        rollupOptions: {
-            // make sure to externalize deps that shouldn't be bundled
-            // into your library
-            external: [
-                "vue",
-                "vuetify",
-                "vuetify/components",
-                "vee-validate",
-                "axios",
-                "pinia",
-                "@qnx/composables",
-                "@qnx/composables/axios",
-                "@tinymce/tinymce-vue",
-                "vuetify/labs/VDataTable",
-                "vuetify/labs/VDatePicker",
-                "vuetify/labs/VOtpInput",
-                "vuetify/labs/VTimePicker"
-            ],
-            output: {
-                //format: 'esm',
-                // Provide global variables to use in the UMD build
-                // for externalized deps
-                globals: {
-                    vue: "Vue"
-                }
-            }
-        }
-    }
+      },
+    },
+  },
 
-    /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
+  /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
   resolve: {
     extensions: [
       '.js',
